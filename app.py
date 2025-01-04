84d4d5544ec49d750a8261037aec1d9c2d76fbac
madiyarkaliyev <kaliyevmadiyar@gmail.com>
2025-01-04 16:38:28 +0500

    add html file

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/hello')
 def hello_world():
     return 'Hello, World! and Madiyar!'
+
+@app.route('/')
+def homepage():
+    return render_template('index.html')
