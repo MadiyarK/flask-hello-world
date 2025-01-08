63da52c9dc04f8717c5e516f907687f273e3d380
Madiyar <kaliyevmafiyar@gmail.com>
2025-01-08 19:02:00 +0600

    adding others

--- app.py
+++ app.py
@@ -2,14 +2,35 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/hello')
 def hello_world():
     return 'Hello, World! and Madiyar!'
+
 
 @app.route('/')
 def homepage():
     return render_template('index.html')
 
+
 @app.route('/Aiganym')
 def aiganym():
     return render_template('Aiganym.html')
+
+
+@app.route('/Mama')
+def mama():
+    return render_template('Mama.html')
+
+
+@app.route('/Papa')
+def papa():
+    return render_template('Papa.html')
+
+@app.route('/Greg')
+def greg():
+    return render_template('Greg.html')
+
+@app.route('/Aari')
+def arai():
+    return render_template('Arai.html')
